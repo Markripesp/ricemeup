0db83413c660856f470b8dca63d50b10fa879280
Mark <35148664+Markripesp@users.noreply.github.com>
2019-03-19 13:15:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo pacman -Syyu
 sudo pacman -S haveged
 sudo systemctl enable haveged.service
 
@@ -12,14 +13,18 @@ sudo systemctl enable haveged.service
 #GNOME
 #sudo pacman -S chrome-gnome-shell gnome-backgrounds gnome-bluetooth gnome-calculator gnome-calendar gnome-disk-utility gnome-font-viewer gnome-screenshot gnome-tweaks simple-scan transmission-gtk
 
-#Thinkpad
-# yay -S thinkfan tlp
-
 #APPS 
-sudo pacman -S chromium gufw timeshift neofetch lolcat otf-ipafont ttf-roboto otf-fira-mono otf-fira-sans
-sudo pacman -S libreoffice-fresh libreoffice-fresh-ca libreoffice-fresh-es pavucontrol
+sudo pacman -Sy chromium
+sudo pacman -S gufw 
+sudo pacman -S neofetch
+sudo pacman -S lolcat 
+sudo pacman -S otf-ipafont ttf-roboto otf-fira-mono otf-fira-sans
+sudo pacman -S libreoffice-fresh libreoffice-fresh-ca libreoffice-fresh-es 
+sudo pacman -S pavucontrol
 sudo pacman -S darktable kdenlive frei0r-plugins breeze-icons qt4 qt5 qt pyqt5-common
-sudo pacman -S vlc steam steam-native-runtime lutris ranger rxvt-unicode ristretto retroarch libretro
+sudo pacman -S vlc steam steam-native-runtime lutris 
+sudo pacman -S ranger rxvt-unicode 
+sudo pacman -S ristretto mupdf
 sudo pacman -S telegram-desktop youtube-dl
 sudo pacman -S htop vim vim-runtime w3m
 sudo pacman -S texlive-most texlive-lang
@@ -30,17 +35,26 @@ sudo pacman -S flatpak
 flatpak install flathub com.spotify.Client
 flatpak install flathub com.skype.Client
 flatpak install flathub com.discordapp.Discord
+
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -S reaper-bin i3lock-fancy 
+yay -S chromium-widevine qt5-webengine-widevine 
+yay -S arch-silence-grub-theme 
+yay -S cava mugshot properties-cpp timeshift timeshift-autosnap
+yay -S tty-clock ulauncher urxvt-font-size-git
+yay -S xcursor-breeze vimix-gtk-themes-git vimix-icon-theme-git
+yay -S nerd-fonts-source-code-pro
 cd /tmp
 git clone https://github.com/alexanderjeurissen/ranger_devicons
 cd ranger_devicons
 make
 sudo make install
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-yay -S reaper-bin i3lock-fancy chromium-widevine qt5-webengine-widevine arch-silence-grub-theme 
-yay -S cava ddgr mugshot properties-cpp timeshift tty-clock ulauncher urxvt-font-size-git
-yay -S xcursor-breeze vimix-gtk-themes-git vimix-icon-theme-git nerd-fonts-source-code-pro
+
+#Thinkpad
+# yay -S thinkfan tlp
 
 #PASTING CONFIG FILES
 sudo rm -f -R ~/.config/ranger
@@ -48,9 +62,11 @@ sudo rm -f ~/.bashrc
 sudo rm -f ~/.Xresources
 sudo rm -f ~/.Xdefaults
 cd /configs
-sudo mv .bashrc ~/
-sudo mv .Xresources ~/
-sudo mv .Xdefaults ~/
+sudo cp bashrc ~/
+sudo cp Xresources ~/
+sudo cp Xdefaults ~/
+sudo cp neofetch.conf ~/.config/neofetch/config.conf
+sudo mv sddm.conf /etc/
 sudo mv ranger ~/.config/
 
 #Mark Ripoll
